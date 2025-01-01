57ec03679ac83c1b125a9c8318fa1a39fb995078
snadi043 <snadi043@uottawa.ca>
2024-12-31 19:38:44 -0500

    Created customized Button component with all the necessary props used to utilized it in the app.

--- components/UI/Input.js
+++ components/UI/Input.js
@@ -1,5 +1,5 @@
 import {View, Text, StyleSheet, TextInput} from 'react-native';
-import { Colors } from '../constants/colors';
+import { Colors } from '../../constants/colors';
 
 export default function Input({
     label, 
