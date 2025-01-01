6204700a6a063b073b23754d2eab52e0e1d2eb5b
snadi043 <snadi043@uottawa.ca>
2025-01-01 12:50:24 -0500

    created a http post method to add the authenticated users to the firebase authentication which is provided by an unique key by the firebase and using this method in the signup screen to verify the users.

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,5 +1,18 @@
 import AuthenticationContent from "../components/AuthenticationContent";
+import { createUser } from "../utilities/http";
+import {useState} from 'react';
 
 export default function SignupScreen(){
-    return(<AuthenticationContent />);
+
+    const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+    async function SignupHandler({email, password}){
+        setIsAuthenticating(true);
+        await createUser(email, password);
+        setIsAuthenticating(false);
+    }
+    if(isAuthenticating){
+        return <LoadingOverlay/>
+    }
+    return(<AuthenticationContent onAuthenticate={SignupHandler}/>);
 }
