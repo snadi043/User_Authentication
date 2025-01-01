9dff97fad8918ca55e6a3c7528b0a63daff898b6
snadi043 <snadi043@uottawa.ca>
2025-01-01 13:26:24 -0500

    Using the createUser function where the post HTTP API method is implemented to add the user authentication and push the user credentials to the firebase authentication backend.

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,6 +1,9 @@
-import AuthenticationContent from "../components/AuthenticationContent";
-import { createUser } from "../utilities/http";
 import {useState} from 'react';
+
+import AuthenticationContent from "../components/AuthenticationContent";
+import LoadingOverlay from '../components/UI/LoadingOverlay';
+import { createUser } from "../utilities/http";
+
 
 export default function SignupScreen(){
 
@@ -12,7 +15,7 @@ export default function SignupScreen(){
         setIsAuthenticating(false);
     }
     if(isAuthenticating){
-        return <LoadingOverlay/>
+        return <LoadingOverlay message="Creating User..."/>
     }
     return(<AuthenticationContent onAuthenticate={SignupHandler}/>);
 }
--- utilities/http.js
+++ utilities/http.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_KEY = 'AIzaSyCYoCUYO3TGavCfKzmD61nCtj_MX84tfwQ';
 
-export async function createUser({email, password}){
+export async function createUser(email, password){
     const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY, 
         {
             email: email,
