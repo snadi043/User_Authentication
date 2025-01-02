f349fe3d38a1a44b24fc6c7f8a6907fe3ae65b06
snadi043 <snadi043@uottawa.ca>
2025-01-02 12:06:51 -0500

    Making the login user features work in the application.

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,5 +1,20 @@
+import { useState } from "react";
 import AuthenticationContent from "../components/AuthenticationContent";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
+import { loginUser } from "../utilities/http";
 
 export default function LoginScreen(){
-    return(<AuthenticationContent isLogin/>);
+
+    const [isAuthenticating, setIsAuthenticating] = useState(false);
+    
+        async function LoginHandler({email, password}){
+            setIsAuthenticating(true);
+            await loginUser(email, password);
+            setIsAuthenticating(false);
+        }
+        if(isAuthenticating){
+            return <LoadingOverlay message="Logging User in..."/>
+        }
+
+    return(<AuthenticationContent isLogin onAuthenticate={LoginHandler}/>);
 }
--- utilities/http.js
+++ utilities/http.js
@@ -2,12 +2,27 @@ import axios from 'axios';
 
 const API_KEY = 'AIzaSyCYoCUYO3TGavCfKzmD61nCtj_MX84tfwQ';
 
-export async function createUser(email, password){
-    const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY, 
-        {
-            email: email,
-            password: password,
-            returnSecureToken: true,
-        }
-    );
+export async function authenticateUser(mode, email, password){
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+    
+    const response = await axios.post(url, 
+    {
+        email: email,
+        password: password,
+        returnSecureToken: true,
+    });
+    // console.log(response.data);
 }
+
+export async function createUser(email, password){
+    authenticateUser('signUp', email, password);
+}
+
+export async function loginUser(email, password){
+    authenticateUser('signInWithPassword', email, password);
+}
+
+
+// Refer to this firebase document on how to create the above functions, necessary changes in the url and HTTP API methods.
+
+// https://firebase.google.com/docs/reference/rest/auth?_gl=1*1o3d91c*_up*MQ..*_ga*NjIyMzY3MDUyLjE3MzU4MzU3Mzg.*_ga_CW55HF8NVT*MTczNTgzNTczNy4xLjAuMTczNTgzNTczNy4wLjAuMA..
